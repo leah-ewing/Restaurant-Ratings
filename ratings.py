a81626b7c13bcd4db3d9ea0a0287a843eac69a07
Leah Ewing <leahewing@live.com>
2021-05-03 20:46:56 +0000

    Final Commit, we did it, actually Shauna did it! :)

--- ratings.py
+++ ratings.py
@@ -1,4 +1,22 @@
 """Restaurant rating lister."""
 
+def rate_restaurant(restaurant_scores):
 
-# put your code here
+    scores_file = open(restaurant_scores)
+    
+
+    restaurant_rating = {}
+    sorted_restaurants = []
+    for restaurant in scores_file:
+        restaurant = restaurant.rstrip()
+        restaurant = restaurant.split(":")
+        sorted_restaurants.append(restaurant)
+    
+    sorted_restaurants = sorted(sorted_restaurants)
+    for restaurant in sorted_restaurants:
+        restaurant_rating[restaurant[0]] = restaurant[1]
+        print(f"{restaurant[0]} is rated at {restaurant[1]}.")
+
+
+
+rate_restaurant("scores.txt")
